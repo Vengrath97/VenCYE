571c5d0539f0ef8da86e898c76ae9aed01b2cfb8
Mikołaj Wojtas <vengrath97@gmail.com>
2022-01-26 18:20:23 +0100

    Add project files.

---
+++ VenCYE/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿
+namespace VenCYE
+{
+    partial class Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxYear1 = new System.Windows.Forms.TextBox();
+            this.LabelYear1 = new System.Windows.Forms.Label();
+            this.LabelMonth1 = new System.Windows.Forms.Label();
+            this.LabelDay1 = new System.Windows.Forms.Label();
+            this.textBoxMonth1 = new System.Windows.Forms.TextBox();
+            this.textBoxDay1 = new System.Windows.Forms.TextBox();
+            this.textBoxExpense1 = new System.Windows.Forms.TextBox();
+            this.textBoxCategory1 = new System.Windows.Forms.TextBox();
+            this.textBoxValue1 = new System.Windows.Forms.TextBox();
+            this.LabelExpense1 = new System.Windows.Forms.Label();
+            this.LabelCategory1 = new System.Windows.Forms.Label();
+            this.LabelValue1 = new System.Windows.Forms.Label();
+            this.ButtonAddExpense1 = new System.Windows.Forms.Button();
+            this.ButtonRemoveExpense1 = new System.Windows.Forms.Button();
+            this.listBoxYear1 = new System.Windows.Forms.ListBox();
+            this.listBoxMonth1 = new System.Windows.Forms.ListBox();
+            this.listBoxDay1 = new System.Windows.Forms.ListBox();
+            this.listBoxExpense1 = new System.Windows.Forms.ListBox();
+            this.listBoxCategory1 = new System.Windows.Forms.ListBox();
+            this.listBoxValue1 = new System.Windows.Forms.ListBox();
+            this.LabelYear2 = new System.Windows.Forms.Label();
+            this.LabelMonth2 = new System.Windows.Forms.Label();
+            this.LabelDay2 = new System.Windows.Forms.Label();
+            this.LabelExpense2 = new System.Windows.Forms.Label();
+            this.LabelCategory2 = new System.Windows.Forms.Label();
+            this.LabelValue2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxYear1
+            // 
+            this.textBoxYear1.Location = new System.Drawing.Point(14, 354);
+            this.textBoxYear1.Name = "textBoxYear1";
+            this.textBoxYear1.Size = new System.Drawing.Size(355, 23);
+            this.textBoxYear1.TabIndex = 0;
+            // 
+            // LabelYear1
+            // 
+            this.LabelYear1.AutoSize = true;
+            this.LabelYear1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LabelYear1.Location = new System.Drawing.Point(12, 330);
+            this.LabelYear1.Name = "LabelYear1";
+            this.LabelYear1.Size = new System.Drawing.Size(43, 21);
+            this.LabelYear1.TabIndex = 2;
+            this.LabelYear1.Text = "Year";
+            // 
+            // LabelMonth1
+            // 
+            this.LabelMonth1.AutoSize = true;
+            this.LabelMonth1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LabelMonth1.Location = new System.Drawing.Point(12, 380);
+            this.LabelMonth1.Name = "LabelMonth1";
+            this.LabelMonth1.Size = new System.Drawing.Size(61, 21);
+            this.LabelMonth1.TabIndex = 3;
+            this.LabelMonth1.Text = "Month";
+            // 
+            // LabelDay1
+            // 
+            this.LabelDay1.AutoSize = true;
+            this.LabelDay1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LabelDay1.Location = new System.Drawing.Point(12, 430);
+            this.LabelDay1.Name = "LabelDay1";
+            this.LabelDay1.Size = new System.Drawing.Size(40, 21);
+            this.LabelDay1.TabIndex = 5;
+            this.LabelDay1.Text = "Day";
+            // 
+            // textBoxMonth1
+            // 
+            this.textBoxMonth1.Location = new System.Drawing.Point(12, 404);
+            this.textBoxMonth1.Name = "textBoxMonth1";
+            this.textBoxMonth1.Size = new System.Drawing.Size(357, 23);
+            this.textBoxMonth1.TabIndex = 6;
+            // 
+            // textBoxDay1
+            // 
+            this.textBoxDay1.Location = new System.Drawing.Point(12, 454);
+            this.textBoxDay1.Name = "textBoxDay1";
+            this.textBoxDay1.Size = new System.Drawing.Size(357, 23);
+            this.textBoxDay1.TabIndex = 7;
+            // 
+            // textBoxExpense1
+            // 
+            this.textBoxExpense1.Location = new System.Drawing.Point(441, 354);
+            this.textBoxExpense1.Name = "textBoxExpense1";
+            this.textBoxExpense1.Size = new System.Drawing.Size(357, 23);
+            this.textBoxExpense1.TabIndex = 8;
+            // 
+            // textBoxCategory1
+            // 
+            this.textBoxCategory1.Location = new System.Drawing.Point(441, 404);
+            this.textBoxCategory1.Name = "textBoxCategory1";
+            this.textBoxCategory1.Size = new System.Drawing.Size(357, 23);
+            this.textBoxCategory1.TabIndex = 9;
+            // 
+            // textBoxValue1
+            // 
+            this.textBoxValue1.Location = new System.Drawing.Point(441, 454);
+            this.textBoxValue1.Name = "textBoxValue1";
+            this.textBoxValue1.Size = new System.Drawing.Size(357, 23);
+            this.textBoxValue1.TabIndex = 10;
+            // 
+            // LabelExpense1
+            // 
+            this.LabelExpense1.AutoSize = true;
+            this.LabelExpense1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LabelExpense1.Location = new System.Drawing.Point(441, 330);
+            this.LabelExpense1.Name = "LabelExpense1";
+            this.LabelExpense1.Size = new System.Drawing.Size(73, 21);
+            this.LabelExpense1.TabIndex = 11;
+            this.LabelExpense1.Text = "Expense";
+            // 
+            // LabelCategory1
+            // 
+            this.LabelCategory1.AutoSize = true;
+            this.LabelCategory1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LabelCategory1.Location = new System.Drawing.Point(441, 380);
+            this.LabelCategory1.Name = "LabelCategory1";
+            this.LabelCategory1.Size = new System.Drawing.Size(80, 21);
+            this.LabelCategory1.TabIndex = 12;
+            this.LabelCategory1.Text = "Category";
+            // 
+            // LabelValue1
+            // 
+            this.LabelValue1.AutoSize = true;
+            this.LabelValue1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LabelValue1.Location = new System.Drawing.Point(441, 430);
+            this.LabelValue1.Name = "LabelValue1";
+            this.LabelValue1.Size = new System.Drawing.Size(53, 21);
+            this.LabelValue1.TabIndex = 13;
+            this.LabelValue1.Text = "Value";
+            // 
+            // ButtonAddExpense1
+            // 
+            this.ButtonAddExpense1.Location = new System.Drawing.Point(441, 483);
+            this.ButtonAddExpense1.Name = "ButtonAddExpense1";
+            this.ButtonAddExpense1.Size = new System.Drawing.Size(115, 61);
+            this.ButtonAddExpense1.TabIndex = 14;
+            this.ButtonAddExpense1.Text = "Add Expense";
+            this.ButtonAddExpense1.UseVisualStyleBackColor = true;
+            // 
+            // ButtonRemoveExpense1
+            // 
+            this.ButtonRemoveExpense1.Location = new System.Drawing.Point(562, 483);
+            this.ButtonRemoveExpense1.Name = "ButtonRemoveExpense1";
+            this.ButtonRemoveExpense1.Size = new System.Drawing.Size(115, 61);
+            this.ButtonRemoveExpense1.TabIndex = 15;
+            this.ButtonRemoveExpense1.Text = "Remove Expense";
+            this.ButtonRemoveExpense1.UseVisualStyleBackColor = true;
+            // 
+            // listBoxYear1
+            // 
+            this.listBoxYear1.FormattingEnabled = true;
+            this.listBoxYear1.ItemHeight = 15;
+            this.listBoxYear1.Location = new System.Drawing.Point(12, 32);
+            this.listBoxYear1.Name = "listBoxYear1";
+            this.listBoxYear1.Size = new System.Drawing.Size(115, 274);
+            this.listBoxYear1.TabIndex = 16;
+            // 
+            // listBoxMonth1
+            // 
+            this.listBoxMonth1.FormattingEnabled = true;
+            this.listBoxMonth1.ItemHeight = 15;
+            this.listBoxMonth1.Location = new System.Drawing.Point(133, 32);
+            this.listBoxMonth1.Name = "listBoxMonth1";
+            this.listBoxMonth1.Size = new System.Drawing.Size(115, 274);
+            this.listBoxMonth1.TabIndex = 17;
+            // 
+            // listBoxDay1
+            // 
+            this.listBoxDay1.FormattingEnabled = true;
+            this.listBoxDay1.ItemHeight = 15;
+            this.listBoxDay1.Location = new System.Drawing.Point(254, 32);
+            this.listBoxDay1.Name = "listBoxDay1";
+            this.listBoxDay1.Size = new System.Drawing.Size(115, 274);
+            this.listBoxDay1.TabIndex = 18;
+            // 
+            // listBoxExpense1
+            // 
+            this.listBoxExpense1.FormattingEnabled = true;
+            this.listBoxExpense1.ItemHeight = 15;
+            this.listBoxExpense1.Location = new System.Drawing.Point(441, 32);
+            this.listBoxExpense1.Name = "listBoxExpense1";
+            this.listBoxExpense1.Size = new System.Drawing.Size(115, 274);
+            this.listBoxExpense1.TabIndex = 19;
+            // 
+            // listBoxCategory1
+            // 
+            this.listBoxCategory1.FormattingEnabled = true;
+            this.listBoxCategory1.ItemHeight = 15;
+            this.listBoxCategory1.Location = new System.Drawing.Point(562, 32);
+            this.listBoxCategory1.Name = "listBoxCategory1";
+            this.listBoxCategory1.Size = new System.Drawing.Size(115, 274);
+            this.listBoxCategory1.TabIndex = 20;
+            // 
+            // listBoxValue1
+            // 
+            this.listBoxValue1.FormattingEnabled = true;
+            this.listBoxValue1.ItemHeight = 15;
+            this.listBoxValue1.Location = new System.Drawing.Point(683, 32);
+            this.listBoxValue1.Name = "listBoxValue1";
+            this.listBoxValue1.Size = new System.Drawing.Size(115, 274);
+            this.listBoxValue1.TabIndex = 21;
+            // 
+            // LabelYear2
+            // 
+            this.LabelYear2.AutoSize = true;
+            this.LabelYear2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LabelYear2.Location = new System.Drawing.Point(14, 9);
+            this.LabelYear2.Name = "LabelYear2";
+            this.LabelYear2.Size = new System.Drawing.Size(43, 21);
+            this.LabelYear2.TabIndex = 22;
+            this.LabelYear2.Text = "Year";
+            // 
+            // LabelMonth2
+            // 
+            this.LabelMonth2.AutoSize = true;
+            this.LabelMonth2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LabelMonth2.Location = new System.Drawing.Point(133, 9);
+            this.LabelMonth2.Name = "LabelMonth2";
+            this.LabelMonth2.Size = new System.Drawing.Size(61, 21);
+            this.LabelMonth2.TabIndex = 23;
+            this.LabelMonth2.Text = "Month";
+            // 
+            // LabelDay2
+            // 
+            this.LabelDay2.AutoSize = true;
+            this.LabelDay2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LabelDay2.Location = new System.Drawing.Point(254, 8);
+            this.LabelDay2.Name = "LabelDay2";
+            this.LabelDay2.Size = new System.Drawing.Size(40, 21);
+            this.LabelDay2.TabIndex = 24;
+            this.LabelDay2.Text = "Day";
+            // 
+            // LabelExpense2
+            // 
+            this.LabelExpense2.AutoSize = true;
+            this.LabelExpense2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LabelExpense2.Location = new System.Drawing.Point(441, 8);
+            this.LabelExpense2.Name = "LabelExpense2";
+            this.LabelExpense2.Size = new System.Drawing.Size(73, 21);
+            this.LabelExpense2.TabIndex = 25;
+            this.LabelExpense2.Text = "Expense";
+            // 
+            // LabelCategory2
+            // 
+            this.LabelCategory2.AutoSize = true;
+            this.LabelCategory2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LabelCategory2.Location = new System.Drawing.Point(562, 9);
+            this.LabelCategory2.Name = "LabelCategory2";
+            this.LabelCategory2.Size = new System.Drawing.Size(80, 21);
+            this.LabelCategory2.TabIndex = 26;
+            this.LabelCategory2.Text = "Category";
+            // 
+            // LabelValue2
+            // 
+            this.LabelValue2.AutoSize = true;
+            this.LabelValue2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LabelValue2.Location = new System.Drawing.Point(683, 9);
+            this.LabelValue2.Name = "LabelValue2";
+            this.LabelValue2.Size = new System.Drawing.Size(53, 21);
+            this.LabelValue2.TabIndex = 27;
+            this.LabelValue2.Text = "Value";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(812, 642);
+            this.Controls.Add(this.LabelValue2);
+            this.Controls.Add(this.LabelCategory2);
+            this.Controls.Add(this.LabelExpense2);
+            this.Controls.Add(this.LabelDay2);
+            this.Controls.Add(this.LabelMonth2);
+            this.Controls.Add(this.LabelYear2);
+            this.Controls.Add(this.listBoxValue1);
+            this.Controls.Add(this.listBoxCategory1);
+            this.Controls.Add(this.listBoxExpense1);
+            this.Controls.Add(this.listBoxDay1);
+            this.Controls.Add(this.listBoxMonth1);
+            this.Controls.Add(this.listBoxYear1);
+            this.Controls.Add(this.ButtonRemoveExpense1);
+            this.Controls.Add(this.ButtonAddExpense1);
+            this.Controls.Add(this.LabelValue1);
+            this.Controls.Add(this.LabelCategory1);
+            this.Controls.Add(this.LabelExpense1);
+            this.Controls.Add(this.textBoxValue1);
+            this.Controls.Add(this.textBoxCategory1);
+            this.Controls.Add(this.textBoxExpense1);
+            this.Controls.Add(this.textBoxDay1);
+            this.Controls.Add(this.textBoxMonth1);
+            this.Controls.Add(this.LabelDay1);
+            this.Controls.Add(this.LabelMonth1);
+            this.Controls.Add(this.LabelYear1);
+            this.Controls.Add(this.textBoxYear1);
+            this.Name = "Form";
+            this.Text = "VenCYE";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxYear1;
+        private System.Windows.Forms.Label LabelYear1;
+        private System.Windows.Forms.Label LabelMonth1;
+        private System.Windows.Forms.Label LabelDay1;
+        private System.Windows.Forms.TextBox textBoxMonth1;
+        private System.Windows.Forms.TextBox textBoxDay1;
+        private System.Windows.Forms.TextBox textBoxExpense1;
+        private System.Windows.Forms.TextBox textBoxCategory1;
+        private System.Windows.Forms.TextBox textBoxValue1;
+        private System.Windows.Forms.Label LabelExpense1;
+        private System.Windows.Forms.Label LabelCategory1;
+        private System.Windows.Forms.Label LabelValue1;
+        private System.Windows.Forms.Button ButtonAddExpense1;
+        private System.Windows.Forms.Button ButtonRemoveExpense1;
+        private System.Windows.Forms.ListBox listBoxYear1;
+        private System.Windows.Forms.ListBox listBoxMonth1;
+        private System.Windows.Forms.ListBox listBoxDay1;
+        private System.Windows.Forms.ListBox listBoxExpense1;
+        private System.Windows.Forms.ListBox listBoxCategory1;
+        private System.Windows.Forms.ListBox listBoxValue1;
+        private System.Windows.Forms.Label LabelYear2;
+        private System.Windows.Forms.Label LabelMonth2;
+        private System.Windows.Forms.Label LabelDay2;
+        private System.Windows.Forms.Label LabelExpense2;
+        private System.Windows.Forms.Label LabelCategory2;
+        private System.Windows.Forms.Label LabelValue2;
+    }
+}
+

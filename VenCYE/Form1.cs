a12694daa47f74b5d646964affa7c1b181703a81
Mikołaj Wojtas <vengrath97@gmail.com>
2022-01-28 14:09:39 +0100

    Cleaned up the code a lot, most important change being the Assign Expense function;
    it's functionalities had been split among functions:
    intValidation();
    doubleValidation();
    getDayIndex();
    
    and 2 new functions AssignExpense, which differ only by type of input data (string vs parsed.ToInt)
    They now also do only what their name suggest - assign an already existing, and passed as input Expense into a day of given date.

--- VenCYE/Form1.cs
+++ VenCYE/Form1.cs
@@ -23,6 +23,27 @@ namespace VenCYE
             }
             InitializeComponent();
         }
+        private void LoadCSV()
+        {
+            string[] lines = File.ReadAllLines(csvFilePath);
+            foreach (string item in lines)
+            {
+                string[] subs = item.Split(",");
+                if (subs[6] == "") { subs[6] = "0"; }
+                string valueString = subs[5] + "," + subs[6];
+                Expense temp = new Expense(subs[3], subs[4], doubleValidation(valueString));
+                AssignExpense(subs[0], subs[1], subs[2], temp);
+            }
+        }
+        private void ClearDisplay()
+        {
+            listBoxYear1.Items.Clear();
+            listBoxMonth1.Items.Clear();
+            listBoxDay1.Items.Clear();
+            listBoxExpense1.Items.Clear();
+            listBoxCategory1.Items.Clear();
+            listBoxValue1.Items.Clear();
+        }
         public void InfoToDisplay()
         {
             foreach (Day day in Days)
@@ -38,28 +59,114 @@ namespace VenCYE
                 }
             }
         }
-        private void ClearDisplay()
+        private int intValidation(string input, int minValue, int maxValue)
         {
-            listBoxYear1.Items.Clear();
-            listBoxMonth1.Items.Clear();
-            listBoxDay1.Items.Clear();
-            listBoxExpense1.Items.Clear();
-            listBoxCategory1.Items.Clear();
-            listBoxValue1.Items.Clear();
+            int parsedValue;
+            if (int.TryParse(input, out parsedValue) && parsedValue >= minValue && parsedValue <= maxValue)
+            {
+                return parsedValue;
+            }
+            else
+            {
+                return -1;
+            }
+        }   //returns value between min and max, or -1 if outside of range, or invalid data
+        private double doubleValidation(string input)
+        {
+            double parsedValue;
+            if (Double.TryParse(input, out parsedValue))
+            {
+                return parsedValue;
+            }
+            else
+            {
+                return -1D;
+            }
+        }                         //returns value, or -1 (in double format) if invalid
+        private int getDayIndex(int day, int month, int year)
+        {
+            int index = 0;
+            foreach (Day dayObject in Days)
+            {
+                if (dayObject.DayOfMonth == day && dayObject.MonthOfYear == month && dayObject.Year == year)
+                {
+                    return index;
+                }
+                index += 1;
+            }
+            return -1;
+        }                 //returns index in Days table, or -1 if day doesn't exist
+        private void AssignExpense(string dayData, string monthData, string yearData, Expense expense)
+        {
+            int parsedDay = intValidation(dayData, 1, 31);
+            int parsedMonth = intValidation(monthData, 1, 12);
+            int parsedYear = intValidation(yearData, 1, 2099);
+
+            bool correctValuesPassed = (!(parsedDay == -1 || parsedMonth == -1 || parsedMonth == -1));
+
+            if (correctValuesPassed)
+            {
+                int index = getDayIndex(parsedDay, parsedMonth, parsedYear);
+
+                if (index >= 0)
+                {
+                    Days[index].AddExpense(expense);
+                }
+                else
+                {
+                    Days.Add(new Day(parsedDay, parsedMonth, parsedYear));
+                    Days[Days.Count - 1].AddExpense(expense);
+                }
+            }
+            else 
+            {
+                MessageBox.Show("Values passed are incorrect!", "Error!");
+            }
+
         }
+        private void AssignExpense(int parsedDay, int parsedMonth, int parsedYear, Expense expense)
+        {
+            bool correctValuesPassed = (!(parsedDay == -1 || parsedMonth == -1 || parsedMonth == -1));
+
+            if (correctValuesPassed)
+            {
+                int index = getDayIndex(parsedDay, parsedMonth, parsedYear);
+
+                if (index >= 0)
+                {
+                    Days[index].AddExpense(expense);
+                }
+                else
+                {
+                    Days.Add(new Day(parsedDay, parsedMonth, parsedYear));
+                    Days[Days.Count - 1].AddExpense(expense);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Values passed are incorrect!", "Error!");
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Days.Clear();
+            ClearDisplay();
+            LoadCSV();
+            InfoToDisplay();
+        }               //Basically restarts the app
         private void ButtonAddExpense1_Click(object sender, EventArgs e)
         {
-            AssignExpense(textBoxDay1.Text, textBoxMonth1.Text, textBoxYear1.Text, textBoxExpense1.Text, textBoxCategory1.Text, textBoxValue1.Text);
+            Expense temp = new Expense(textBoxExpense1.Text, textBoxCategory1.Text, doubleValidation(textBoxValue1.Text));
+            AssignExpense(textBoxDay1.Text, textBoxMonth1.Text, textBoxYear1.Text, temp);
             ClearDisplay();
             InfoToDisplay();
         }
-
         private void ButtonRemoveExpense1_Click(object sender, EventArgs e)
         {
             ClearDisplay();
             Days.Clear();
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
 
@@ -68,67 +175,6 @@ namespace VenCYE
             {
                 day.SaveToCSV();
             }
-        }
-        private void LoadCSV()
-        {
-            string[] lines = File.ReadAllLines(csvFilePath);
-            foreach (string item in lines)
-            {
-                string[] subs = item.Split(",");
-                if (subs[6] == "") { subs[6] = "0"; }
-                string valueString = subs[5] + "," + subs[6];
-                AssignExpense(subs[0], subs[1], subs[2], subs[3], subs[4], valueString);
-            }
-        }
-        private void AssignExpense(string daydata, string monthdata, string yeardata, string expensedata, string categorydata, string valuedata)
-        {
-            int parsedDay;
-            bool dayValid = false;
-            if (int.TryParse(daydata, out parsedDay) && parsedDay < 32) { dayValid = true; }
-
-            int parsedMonth;
-            bool monthValid = false;
-            if (int.TryParse(monthdata, out parsedMonth) && parsedMonth < 13 && 0 < parsedMonth) { monthValid = true; }
-
-            int parsedYear;
-            bool yearValid = false;
-            if (int.TryParse(yeardata, out parsedYear) && 0 < parsedDay) { yearValid = true; }
-
-            double parsedValue;
-            bool valueValid = false;
-            if (Double.TryParse(valuedata, out parsedValue) && parsedValue > 0) { valueValid = true; }
-
-            if (dayValid && monthValid && yearValid && valueValid)
-            {
-                bool dayAlreadyExists = false;
-                foreach (Day day in Days)
-                {
-                    if (day.DayOfMonth == parsedDay && day.MonthOfYear == parsedMonth && day.Year == parsedYear)
-                    {
-                        dayAlreadyExists = true;
-                        day.AddExpense(new Expense(expensedata, categorydata, parsedValue));
-                        break;
-
-                    }
-                }
-                if (!dayAlreadyExists)
-                {
-                    Days.Add(new Day(parsedDay, parsedMonth, parsedYear));
-                    Days[Days.Count - 1].AddExpense(new Expense(expensedata, categorydata, parsedValue));
-                }
-            }
-            else
-            {
-                MessageBox.Show("Not all required fields are properly filled!", "Error!", MessageBoxButtons.OK);
-            }
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Days.Clear();
-            ClearDisplay();
-            LoadCSV();
-            InfoToDisplay();
         }
     }
 }
